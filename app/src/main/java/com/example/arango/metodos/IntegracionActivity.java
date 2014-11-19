eec55b65e8d1831ed80f67d5e7f4362297e24821
felipegi91 <felipegi91@gmail.com>
2014-11-18 19:43:09 -0500

    array spinner

--- app/src/main/java/com/example/arango/metodos/IntegracionActivity.java
+++ app/src/main/java/com/example/arango/metodos/IntegracionActivity.java
@@ -2,6 +2,8 @@ package com.example.arango.metodos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by Felipe on 18/11/2014.
@@ -10,5 +12,15 @@ public class IntegracionActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_integracion);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerIntegracion);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.integracion, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/arango/metodos/InterpolacionActivity.java
+++ app/src/main/java/com/example/arango/metodos/InterpolacionActivity.java
@@ -2,6 +2,8 @@ package com.example.arango.metodos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by Felipe on 18/11/2014.
@@ -10,5 +12,15 @@ public class InterpolacionActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interpolacion);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerInterpolacion);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.interpolacion, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
