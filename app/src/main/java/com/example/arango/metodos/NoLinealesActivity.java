e07a4c4033891523794622043a12902685f1d5f9
felipegi91 <felipegi91@gmail.com>
2014-11-19 15:07:29 -0500

    Ecuaciones No lineales OK.

--- app/src/main/java/com/example/arango/metodos/NoLinealesActivity.java
+++ app/src/main/java/com/example/arango/metodos/NoLinealesActivity.java
@@ -21,19 +21,21 @@ import org.w3c.dom.Text;
  */
 public class NoLinealesActivity extends Activity {
     private static int metodo_a_usar;
-    private EditText edtxtFuncion, edtxtValorInicial, edtxtDelta, edtxtIteraciones, edtxtAd1 ,edtxtAd2;
-    private TextView txtResult;
+    private EditText edtxtFuncion, edtxtValorInicial, edtxtDelta, edtxtIteraciones, edtxtAd1 ,edtxtTol, edtxtAd3;
+    private TextView txtResult, txtColumName;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nolineales);
 
         metodo_a_usar = -1;
+        txtColumName = (TextView) findViewById(R.id.txtColumName);
         edtxtFuncion = (EditText) findViewById(R.id.edtxtNoLnFuncion);
         edtxtValorInicial = (EditText) findViewById(R.id.editTextValorInicial);
         edtxtDelta = (EditText) findViewById(R.id.editTextDelta);
         edtxtIteraciones = (EditText) findViewById(R.id.editTextIteraciones);
         edtxtAd1 = (EditText) findViewById(R.id.edtxtAd1);
-        edtxtAd2 = (EditText) findViewById(R.id.edtxtAd2);
+        edtxtTol = (EditText) findViewById(R.id.edtxtAd2);
+        edtxtAd3 = (EditText) findViewById(R.id.edtxtAd3);
         txtResult = (TextView) findViewById(R.id.txtNoLinealesResult);
 
         Spinner spinner = (Spinner) findViewById(R.id.spinnerNoLineales);
@@ -50,55 +52,76 @@ public class NoLinealesActivity extends Activity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 metodo_a_usar = i;
+                edtxtAd1.setText("");
+                edtxtTol.setText("");
+                edtxtAd3.setText("");
+                edtxtValorInicial.setText("");
+                edtxtDelta.setText("");
+                edtxtIteraciones.setText("");
                 switch (i){
                     case 0:
+                        txtColumName.setText(" Xi  F(Xi)");
                         edtxtAd1.setVisibility(View.GONE);
-                        edtxtAd2.setVisibility(View.GONE);
-                        edtxtIteraciones.setVisibility(View.VISIBLE);
+                        edtxtTol.setVisibility(View.GONE);
+                        edtxtAd3.setVisibility(View.GONE);
                         edtxtDelta.setVisibility(View.VISIBLE);
                         break;
                     case 1:
+                        txtColumName.setText(" N  Xinf  Xsup  Xm  F(Xm)   Error");
                         edtxtAd1.setHint("Valor Siguiente");
-                        edtxtAd2.setHint("Tolerancia");
+                        edtxtTol.setHint("Tolerancia");
+                        edtxtAd3.setVisibility(View.GONE);
                         edtxtDelta.setVisibility(View.GONE);
                         edtxtAd1.setVisibility(View.VISIBLE);
-                        edtxtAd2.setVisibility(View.VISIBLE);
+                        edtxtTol.setVisibility(View.VISIBLE);
                         break;
                     case 2:
+                        txtColumName.setText(" N  Xinf  Xsup  Xm  F(Xm)   Error");
                         edtxtAd1.setHint("Valor Siguiente");
-                        edtxtAd2.setHint("Tolerancia");
+                        edtxtTol.setHint("Tolerancia");
+                        edtxtAd3.setVisibility(View.GONE);
+                        edtxtDelta.setVisibility(View.GONE);
                         edtxtAd1.setVisibility(View.VISIBLE);
-                        edtxtAd2.setVisibility(View.VISIBLE);
+                        edtxtTol.setVisibility(View.VISIBLE);
                         break;
                     case 3:
-                        edtxtAd1.setHint("g'(x)");
-                        edtxtAd2.setHint("Tolerancia");
+                        txtColumName.setText(" N  Xn  F(Xn)  Error");
+                        edtxtAd1.setHint("g(x)");
+                        edtxtTol.setHint("Tolerancia");
+                        edtxtDelta.setVisibility(View.GONE);
+                        edtxtAd3.setVisibility(View.GONE);
                         edtxtAd1.setVisibility(View.VISIBLE);
-                        edtxtAd2.setVisibility(View.VISIBLE);
+                        edtxtTol.setVisibility(View.VISIBLE);
                         break;
                     case 4:
+                        txtColumName.setText(" N  Xn  F(Xn)  Error");
                         edtxtAd1.setHint("f'(x)");
-                        edtxtAd2.setHint("Tolerancia");
+                        edtxtTol.setHint("Tolerancia");
+                        edtxtDelta.setVisibility(View.GONE);
+                        edtxtAd3.setVisibility(View.GONE);
                         edtxtAd1.setVisibility(View.VISIBLE);
-                        edtxtAd2.setVisibility(View.VISIBLE);
+                        edtxtTol.setVisibility(View.VISIBLE);
                         break;
                     case 5:
-                        edtxtAd1.setHint("g'(x)");
-                        edtxtAd2.setHint("Tolerancia");
+                        txtColumName.setText(" N  Xn  F(Xn)  Error");
+                        edtxtAd1.setHint("Valor Siguiente");
+                        edtxtTol.setHint("Tolerancia");
+                        edtxtAd3.setVisibility(View.GONE);
+                        edtxtDelta.setVisibility(View.GONE);
                         edtxtAd1.setVisibility(View.VISIBLE);
-                        edtxtAd2.setVisibility(View.VISIBLE);
+                        edtxtTol.setVisibility(View.VISIBLE);
                         break;
                     case 6:
-                        edtxtAd1.setHint("Valor Siguiente");
-                        edtxtAd2.setHint("Tolerancia");
+                        txtColumName.setText(" N  Xn  F(Xn)  F'(Xn)  F''(Xn)  Error");
+                        edtxtAd1.setHint("f'(x)");
+                        edtxtAd3.setHint("f''(x)");
+                        edtxtTol.setHint("Tolerancia");
+                        edtxtDelta.setVisibility(View.GONE);
                         edtxtAd1.setVisibility(View.VISIBLE);
-                        edtxtAd2.setVisibility(View.VISIBLE);
+                        edtxtTol.setVisibility(View.VISIBLE);
+                        edtxtAd3.setVisibility(View.VISIBLE);
                         break;
                     default:
-                        edtxtAd1.setHint("Tolerancia");
-                        edtxtAd2.setHint("Iteraciones");
-                        edtxtAd1.setVisibility(View.VISIBLE);
-                        edtxtAd2.setVisibility(View.VISIBLE);
                         break;
                 }
             }
@@ -120,48 +143,52 @@ public class NoLinealesActivity extends Activity {
                         EcNoLineales ecunl = new EcNoLineales(edtxtFuncion.getText().toString().trim());
                         double x0 = Double.parseDouble(edtxtValorInicial.getText().toString().trim());
                         int iter = Integer.parseInt(edtxtIteraciones.getText().toString().trim());
-                        double delta = Double.parseDouble(edtxtDelta.getText().toString().trim());
                         double[][] tabla;
                         String result;
                         switch (metodo_a_usar) {
                             case 0:
-                                ecunl.busquedas(x0, delta, iter);
+                                ecunl.busquedas(x0, Double.parseDouble(edtxtDelta.getText().toString().trim()), iter);
                                 tabla = ecunl.getTabla();
                                 result = getResult(tabla);
                                 txtResult.setText(result);
                                 break;
                             case 1:
-                                ecunl.biseccion(x0, Double.parseDouble(edtxtAd1.getText().toString()), Double.parseDouble(edtxtAd2.getText().toString()), iter);
+                                ecunl.biseccion(x0, Double.parseDouble(edtxtAd1.getText().toString()), Double.parseDouble(edtxtTol.getText().toString()), iter);
                                 tabla = ecunl.getTabla();
                                 result = getResult(tabla);
                                 txtResult.setText(result);
                                 break;
                             case 2:
-                                ecunl.reglaFalsa(x0, Double.parseDouble(edtxtAd1.getText().toString()), Double.parseDouble(edtxtAd2.getText().toString()), iter);
+                                ecunl.reglaFalsa(x0, Double.parseDouble(edtxtAd1.getText().toString()), Double.parseDouble(edtxtTol.getText().toString()), iter);
                                 tabla = ecunl.getTabla();
                                 result = getResult(tabla);
                                 txtResult.setText(result);
                                 break;
                             case 3:
-                                //ecunl.puntoFijo();
+                                ecunl.setgx( edtxtAd1.getText().toString());
+                                ecunl.puntoFijo(x0, Double.parseDouble(edtxtTol.getText().toString()), iter);
                                 tabla = ecunl.getTabla();
                                 result = getResult(tabla);
                                 txtResult.setText(result);
                                 break;
                             case 4:
-                                //ecunl.newton();
+                                ecunl.setdfx( edtxtAd1.getText().toString());
+                                //x0 | f'(x) | tol | iter
+                                ecunl.newton(x0,  Double.parseDouble(edtxtTol.getText().toString()) ,iter);
                                 tabla = ecunl.getTabla();
                                 result = getResult(tabla);
                                 txtResult.setText(result);
                                 break;
                             case 5:
-                                //ecunl.secante();
+                                ecunl.secante(x0,  Double.parseDouble(edtxtAd1.getText().toString()), Double.parseDouble(edtxtTol.getText().toString()), iter);
                                 tabla = ecunl.getTabla();
                                 result = getResult(tabla);
                                 txtResult.setText(result);
                                 break;
                             case 6:
-                                //ecunl.raicesMulti();
+                                ecunl.setdfx( edtxtAd1.getText().toString());
+                                ecunl.setddfx( edtxtAd3.getText().toString());
+                                ecunl.raicesMulti(x0,  Double.parseDouble(edtxtTol.getText().toString()) ,iter);
                                 tabla = ecunl.getTabla();
                                 result = getResult(tabla);
                                 txtResult.setText(result);
@@ -177,7 +204,7 @@ public class NoLinealesActivity extends Activity {
         String result = "";
         for(int fila = 0; fila < tabla.length; fila++){
             for(int columna = 0; columna < tabla[0].length; columna++){
-                result += " "+String.valueOf(tabla[fila][columna]);
+                result += "   "+String.valueOf(tabla[fila][columna]);
             }
             result += "\n";
         }
